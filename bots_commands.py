4710e891908a4fb511581556e6a580e563d60600
Albert <albert.fedorov.99@bk.ru>
2023-01-30 17:50:24 +0300

    Модуль команд, которые может выполнять бот

---
+++ bots_commands.py
@@ -0,0 +1,21 @@
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+import datetime
+
+async def hello(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text(f'Hello {update.effective_user.first_name}!')
+
+async def help(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text(f'/hello\n/help\n/sum\n/new_year')
+
+async def sum(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    msg = update.message.text
+    items = msg.split()
+    x = int(items[1])
+    y = int(items[3])
+    await update.message.reply_text(f'{x} + {y} = {x + y}')
+async def new_year(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    now_date = datetime.date.today()
+    next_date = datetime.date(2024, 1, 1)
+    rem_date = next_date - now_date
+    await update.message.reply_text(f'До Нового Года осталось: {rem_date}')
